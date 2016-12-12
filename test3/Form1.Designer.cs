7f5bdf85918f822b2f8f25d1666e7ff50803a55a
raman23011997 <sramanjeet57@gmail.com>
2016-12-12 10:00:11 -0500

    New

---
+++ test3/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace test3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblerr2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblerr1 = new System.Windows.Forms.Label();
+            this.lblres = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblpas = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.lblQ7 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblerr2
+            // 
+            this.lblerr2.AutoSize = true;
+            this.lblerr2.ForeColor = System.Drawing.Color.Orange;
+            this.lblerr2.Location = new System.Drawing.Point(206, 178);
+            this.lblerr2.Name = "lblerr2";
+            this.lblerr2.Size = new System.Drawing.Size(63, 13);
+            this.lblerr2.TabIndex = 4;
+            this.lblerr2.Text = "invalid input";
+            this.lblerr2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblerr1
+            // 
+            this.lblerr1.AutoSize = true;
+            this.lblerr1.ForeColor = System.Drawing.Color.Orange;
+            this.lblerr1.Location = new System.Drawing.Point(206, 126);
+            this.lblerr1.Name = "lblerr1";
+            this.lblerr1.Size = new System.Drawing.Size(63, 13);
+            this.lblerr1.TabIndex = 3;
+            this.lblerr1.Text = "invalid input";
+            this.lblerr1.Visible = false;
+            // 
+            // lblres
+            // 
+            this.lblres.AutoSize = true;
+            this.lblres.Location = new System.Drawing.Point(116, 245);
+            this.lblres.Name = "lblres";
+            this.lblres.Size = new System.Drawing.Size(0, 13);
+            this.lblres.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(83, 350);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(100, 20);
+            this.txtpassword.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(108, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblpas
+            // 
+            this.lblpas.AutoSize = true;
+            this.lblpas.Location = new System.Drawing.Point(80, 431);
+            this.lblpas.Name = "lblpas";
+            this.lblpas.Size = new System.Drawing.Size(0, 13);
+            this.lblpas.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(388, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(391, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Spring ",
+            "Summer ",
+            " Fall ",
+            "Winter"});
+            this.comboBox1.Location = new System.Drawing.Point(391, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(391, 236);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(423, 277);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 24);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "show months";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // lblQ7
+            // 
+            this.lblQ7.AutoSize = true;
+            this.lblQ7.Location = new System.Drawing.Point(423, 327);
+            this.lblQ7.Name = "lblQ7";
+            this.lblQ7.Size = new System.Drawing.Size(0, 13);
+            this.lblQ7.TabIndex = 17;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(391, 347);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 488);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.lblQ7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblpas);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblres);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblerr2);
+            this.Controls.Add(this.lblerr1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblerr2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblerr1;
+        private System.Windows.Forms.Label lblres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblpas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lblQ7;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ test3/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "my name is ramanjeet singh,my student number is 200349853 and i nam going to do great in this exam";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int32 one, two;
+            //Int32 one = Convert.ToInt32(textBox1.Text);
+            //Int32 two= Convert.ToInt32(textBox2.Text);
+            Boolean ok = true;
+
+            if (Int32.TryParse(textBox1.Text, out one) == false)
+            {
+                lblerr1.Visible = true;
+                ok = false;
+
+            }
+
+            if (Int32.TryParse(textBox2.Text, out two) == false)
+            {
+                lblerr2.Visible = true;
+                ok = false;
+            }
+            if (ok == true)
+            {
+                Int32 result = multinum(one, two);
+                lblres.Text = result.ToString();
+            }
+
+
+        }
+        private Int32 multinum(Int32 one, Int32 two)
+        {
+            Int32 result = one * two;
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            //333///
+            String[] display = { "Merry ", "christmas", "c# is great" };
+            for (var i = 0; i < 3; i++)
+            {
+                label3.Text += display[i];
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string password = txtpassword.Text;
+            if (txtpassword.Text == "frosty")
+            {
+                lblpas.ForeColor = Color.Green;
+                lblpas.Text = "You entered the correct password";
+            }
+            else
+            {
+                lblpas.ForeColor = Color.Red;
+                lblpas.Text = "You entered the incorrect password";
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[,] MOVIES = new string[3, 3];
+            MOVIES[0, 0] = "	How the Grinch Stole Christmas";
+            MOVIES[0, 1] = "1966\n";
+            MOVIES[1, 0] = "Frosty the Snowman ";
+            MOVIES[1, 1] = "1969\n";
+            MOVIES[2, 0] = "Arthur Christmas ";
+            MOVIES[2, 1] = "2011\n";
+
+            for (var i = 0; i < 3; i++)
+            {
+                label4.Text += MOVIES[i, 0];
+                label4.Text += MOVIES[i, 1];
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Double x = 4;
+            Double y = 12;
+            Double result = x / y;
+
+            lblQ7.Text = result.ToString();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == Spring)
+            {
+
+                comboBox2.Text = "March\n,April\n,May\n";
+            }
+            else
+                 if (comboBox1.SelectedItem == Summer)
+            {
+
+                comboBox2.Text = "june\n,july\n,august\n";
+
+            }
+            else
+                 if (comboBox1.SelectedItem == fall)
+            {
+
+                comboBox2.Text = "sept\n,oct\n,nov\n";
+
+            }
+            else
+                 if (comboBox1.SelectedItem == Winter)
+            {
+
+                comboBox2.Text = "dec\n,jan\n,feb\n";
+
+            }
+            }
+    }
+}
